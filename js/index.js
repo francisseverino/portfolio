280a6fc1f6c52606b35796641207bdbb9e7092bb
Francis Severino <francisjsg2@gmail.com>
2021-03-06 22:14:41 -0500

    nav indicator on scroll

--- js/index.js
+++ js/index.js
@@ -47,3 +47,28 @@ document.addEventListener('DOMContentLoaded', function (event) {
     document.documentElement.setAttribute('data-theme', switchToTheme);
   };
 });
+
+// Nav indicator highlight on scroll
+const navIndicatorOnScroll = () => {
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('.nav__link');
+  window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute('id');
+      }
+    });
+
+    navLinks.forEach(li => {
+      li.classList.remove('active');
+      if (li.href.includes(current)) {
+        li.classList.add('active');
+      }
+    });
+  });
+};
+
+navIndicatorOnScroll();
