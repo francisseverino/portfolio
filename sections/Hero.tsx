26a704499087ba452e050629e68d115b2542cb68
Francis Severino <francisjsg2@gmail.com>
2025-02-24 13:44:00 -0500

    add grain and hero ring in hero page for more interesting UI

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import memojiImage from '@/assets/memoji.png';
 import ArrowDown from '@/assets/icons/arrow-down.svg';
 import Image from 'next/image';
+import grainImage from '@/assets/grain.jpg';
 
 const Hero = () => {
   return (
-    <div className='py-32 md:py-48 lg:py-60'>
+    <div className='py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip'>
+      <div
+        className='absolute inset-0 -z-30 opacity-5'
+        style={{ backgroundImage: `url(${grainImage.src})` }}></div>
+      <div className=' size-[620px] hero-ring'></div>
+      <div className=' size-[820px] hero-ring'></div>
+      <div className=' size-[1020px] hero-ring'></div>
+      <div className=' size-[1220px] hero-ring'></div>
       <div className='container'>
         <div className='flex flex-col items-center'>
           <Image
