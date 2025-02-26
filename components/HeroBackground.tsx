b8f1ca07d1bcdd1913c8272689bc47c533e01282
Francis Severino <francisjsg2@gmail.com>
2025-02-25 21:07:34 -0500

    move hero background to its own component

---
+++ components/HeroBackground.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+
+import grainImage from '@/assets/grain.jpg';
+import StarIcon from '@/assets/icons/star.svg';
+import SparkleIcon from '@/assets/icons/sparkle.svg';
+import HeroOrbit from '@/components/HeroOrbit';
+
+const HeroBackground = () => {
+  return (
+    <div className='absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]'>
+      <div
+        className='absolute inset-0 -z-30 opacity-5'
+        style={{ backgroundImage: `url(${grainImage.src})` }}
+      />
+
+      <div className=' size-[620px] hero-ring' />
+      <div className=' size-[820px] hero-ring' />
+      <div className=' size-[1020px] hero-ring' />
+      <div className=' size-[1220px] hero-ring' />
+
+      <HeroOrbit
+        size={430}
+        rotation={-14}
+        shouldOrbit
+        orbitDuration={30}
+        shouldSpin
+        spinDuration={3}>
+        <SparkleIcon className='size-8 text-emerald-300/20' />
+      </HeroOrbit>
+      <HeroOrbit
+        size={440}
+        rotation={79}
+        shouldOrbit
+        orbitDuration={32}
+        shouldSpin
+        spinDuration={3}>
+        <SparkleIcon className='size-5 text-emerald-300/20' />
+      </HeroOrbit>
+      <HeroOrbit size={520} rotation={-41} shouldOrbit orbitDuration={34}>
+        <div className='size-2 rounded-full bg-emerald-300/20' />
+      </HeroOrbit>
+      <HeroOrbit
+        size={530}
+        rotation={178}
+        shouldOrbit
+        orbitDuration={36}
+        shouldSpin
+        spinDuration={3}>
+        <SparkleIcon className='size-10 text-emerald-300/20' />
+      </HeroOrbit>
+      <HeroOrbit
+        size={550}
+        rotation={20}
+        shouldOrbit
+        orbitDuration={38}
+        shouldSpin
+        spinDuration={6}>
+        <StarIcon className='size-12 text-emerald-300' />
+      </HeroOrbit>
+      <HeroOrbit
+        size={590}
+        rotation={98}
+        shouldOrbit
+        orbitDuration={40}
+        shouldSpin
+        spinDuration={6}>
+        <StarIcon className='size-8 text-emerald-300' />
+      </HeroOrbit>
+      <HeroOrbit size={650} rotation={-5} shouldOrbit orbitDuration={42}>
+        <div className='size-2 rounded-full bg-emerald-300/20' />
+      </HeroOrbit>
+      <HeroOrbit
+        size={710}
+        rotation={144}
+        shouldOrbit
+        orbitDuration={44}
+        shouldSpin
+        spinDuration={6}>
+        <SparkleIcon className='size-14 text-emerald-300/20' />
+      </HeroOrbit>
+      <HeroOrbit
+        size={720}
+        rotation={85}
+        shouldOrbit
+        orbitDuration={46}
+        shouldSpin
+        spinDuration={6}>
+        <div className='size-3 rounded-full bg-emerald-300/20' />
+      </HeroOrbit>
+      <HeroOrbit
+        size={800}
+        rotation={-72}
+        shouldOrbit
+        orbitDuration={48}
+        shouldSpin
+        spinDuration={6}>
+        <StarIcon className='size-28 text-emerald-300' />
+      </HeroOrbit>
+    </div>
+  );
+};
+
+export default HeroBackground;
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -2,105 +2,12 @@ import React from 'react';
 import memojiImage from '@/assets/memoji.png';
 import ArrowDown from '@/assets/icons/arrow-down.svg';
 import Image from 'next/image';
-import grainImage from '@/assets/grain.jpg';
-import StarIcon from '@/assets/icons/star.svg';
-import SparkleIcon from '@/assets/icons/sparkle.svg';
-import HeroOrbit from '@/components/HeroOrbit';
+import HeroBackground from '@/components/HeroBackground';
 
 const Hero = () => {
   return (
     <div className='py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip'>
-      <div className='absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]'>
-        <div
-          className='absolute inset-0 -z-30 opacity-5'
-          style={{ backgroundImage: `url(${grainImage.src})` }}
-        />
-
-        <div className=' size-[620px] hero-ring' />
-        <div className=' size-[820px] hero-ring' />
-        <div className=' size-[1020px] hero-ring' />
-        <div className=' size-[1220px] hero-ring' />
-
-        <HeroOrbit
-          size={430}
-          rotation={-14}
-          shouldOrbit
-          orbitDuration={30}
-          shouldSpin
-          spinDuration={3}>
-          <SparkleIcon className='size-8 text-emerald-300/20' />
-        </HeroOrbit>
-        <HeroOrbit
-          size={440}
-          rotation={79}
-          shouldOrbit
-          orbitDuration={32}
-          shouldSpin
-          spinDuration={3}>
-          <SparkleIcon className='size-5 text-emerald-300/20' />
-        </HeroOrbit>
-        <HeroOrbit size={520} rotation={-41} shouldOrbit orbitDuration={34}>
-          <div className='size-2 rounded-full bg-emerald-300/20' />
-        </HeroOrbit>
-        <HeroOrbit
-          size={530}
-          rotation={178}
-          shouldOrbit
-          orbitDuration={36}
-          shouldSpin
-          spinDuration={3}>
-          <SparkleIcon className='size-10 text-emerald-300/20' />
-        </HeroOrbit>
-        <HeroOrbit
-          size={550}
-          rotation={20}
-          shouldOrbit
-          orbitDuration={38}
-          shouldSpin
-          spinDuration={6}>
-          <StarIcon className='size-12 text-emerald-300' />
-        </HeroOrbit>
-        <HeroOrbit
-          size={590}
-          rotation={98}
-          shouldOrbit
-          orbitDuration={40}
-          shouldSpin
-          spinDuration={6}>
-          <StarIcon className='size-8 text-emerald-300' />
-        </HeroOrbit>
-        <HeroOrbit size={650} rotation={-5} shouldOrbit orbitDuration={42}>
-          <div className='size-2 rounded-full bg-emerald-300/20' />
-        </HeroOrbit>
-        <HeroOrbit
-          size={710}
-          rotation={144}
-          shouldOrbit
-          orbitDuration={44}
-          shouldSpin
-          spinDuration={6}>
-          <SparkleIcon className='size-14 text-emerald-300/20' />
-        </HeroOrbit>
-        <HeroOrbit
-          size={720}
-          rotation={85}
-          shouldOrbit
-          orbitDuration={46}
-          shouldSpin
-          spinDuration={6}>
-          <div className='size-3 rounded-full bg-emerald-300/20' />
-        </HeroOrbit>
-        <HeroOrbit
-          size={800}
-          rotation={-72}
-          shouldOrbit
-          orbitDuration={48}
-          shouldSpin
-          spinDuration={6}>
-          <StarIcon className='size-28 text-emerald-300' />
-        </HeroOrbit>
-      </div>
-
+      <HeroBackground />
       <div className='container'>
         <div className='flex flex-col items-center'>
           <Image
